b129f822f5cf01e20a5a0afe64e03dc2640318eb
AnKr <ankr@alice.de>
2023-05-21 21:10:01 +0200

    added homescreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,7 +29,7 @@ function App() {
           >
             Learn React
           </a>
-          {" | "}
+          {'| '}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -45,4 +44,34 @@ function App() {
   );
 }
 
-export default App;
+export default App() {
+  return (
+    <div className="bg-bgdark height: 100%">
+      <div className="mx-5 flex-col justify-center text-white">
+        <h1 className="text-4xl font-bold">Hi Name!</h1>
+        <div className="flex justify-center">
+          <img className="w-9/12" src="./images/homescreen.svg" />
+        </div>
+        <div className="mt-11 flex justify-between items-end">
+          <h2 className="text-3xl font-bold">Dein Workout heute:</h2>
+          <p className="font-normal text-xs">Trainingsplan</p>
+        </div>
+        <div className="h-52 w-full bg-bggrey rounded-3xl">
+          <a href={`/Trainingsprogrammdetails`}>
+            <div className="h-full m-7 flex flex-col justify-end">
+              <h3 className="text-lg font-bold leading-7">Tag 2</h3>
+              <h3 className="text-2xl font-bold leading-9">
+              Titel des Programms 
+              </h3>
+              <p className="mb-7 text-xs">26 Minuten - Beweglichkeit</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div>
+        <p>Navbar</p>
+      </div>
+    </div>
+ );
+
+}
